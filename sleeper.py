50b7f42cd1617c056ed498ddf757d19493f2ed30
spectraltoast1 <88110329+spectraltoast1@users.noreply.github.com>
2026-04-25 21:35:51 -0400

    End the day 4.25.26

--- sleeper.py
+++ sleeper.py
@@ -5,6 +5,23 @@ def get_sleeper_user(username):
     r = requests.get(url)
     return r.json()
 
+
+def get_sleeper_leagues(user_id):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     user = get_sleeper_user("spectraltoast1")
     print(user)
