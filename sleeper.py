487099d2b446c955b1e58becb438e28fe2802f25
spectraltoast1 <88110329+spectraltoast1@users.noreply.github.com>
2026-04-25 20:14:11 -0400

    Add name = main guardrail

--- sleeper.py
+++ sleeper.py
@@ -4,3 +4,7 @@ def get_sleeper_user(username):
     url = f"https://api.sleeper.app/v1/user/{username}"
     r = requests.get(url)
     return r.json()
+
+if __name__ == "__main__":
+    user = get_sleeper_user("spectraltoast1")
+    print(user)
